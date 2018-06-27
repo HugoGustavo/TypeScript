6ec4f9e89fc2e6d6948f05a548cb317c777d7881
Hugo Gustavo <hugog.valin@gmail.com>
2018-06-27 17:16:54 -0300

    Variables

--- variables.ts
+++ variables.ts
@@ -1,4 +1,7 @@
-var name:string = "John";
+const nome:string = "John";
 var score1:number = 50;
 var score2:number = 42.5;
 var sum = score1 + score2;
+console.log("first score: " + score1);
+console.log("second score: " + score2);
+console.log("sum of scores: " + sum); 
