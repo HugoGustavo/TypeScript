feb8d95d503f87f777782bd9c5f684df481abead
Hugo Gustavo <hugog.valin@gmail.com>
2018-06-27 17:22:01 -0300

    Modificando AssertionTypes

---
+++ variables.js
@@ -0,0 +1,7 @@
+var nome = "John";
+var score1 = 50;
+var score2 = 42.5;
+var sum = score1 + score2;
+console.log("first score: " + score1);
+console.log("second score: " + score2);
+console.log("sum of scores: " + sum);
