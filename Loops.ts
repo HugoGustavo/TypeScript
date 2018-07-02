0e5965382e1365cc870078af468bc5ddf3280f25
Hugo Gustavo <hugog.valin@gmail.com>
2018-07-02 07:35:00 -0300

    Examplo de Loops

--- Loops.ts
+++ Loops.ts
@@ -7,3 +7,41 @@ for(i = num; i >= 1; i--){
 }
 
 console.log(factorial);
+
+var j:any;
+var n:any = "a b c";
+for (j in n){
+    console.log(n[j]);
+}
+
+num = 5;
+factorial = 1;
+while(num >= 1){
+    factorial =  factorial * num;
+    num--;
+}
+console.log("The factorial is " + factorial);
+
+num = 10;
+do {
+    console.log(num);
+    num--;
+} while ( num >= 0 );
+
+var i:number = 1;
+while ( i <= 10 ){
+    if ( i % 5 == 0){
+        console.log("The first multiple of 5 between 1 and 10 is: " + i);
+        break;
+    }
+    i++;
+}
+
+num = 0;
+var count:number = 0;
+for ( num=0; num <=20; num++){
+    if ( num % 2 == 0 )
+        continue;
+    count++;
+}
+console.log("The count of odd values between 0 and 20 is: " + count);
